43bf1f70733cf099895dcfd930543f44380c86cf
RaymonOMG <97995339+RaymonOMG@users.noreply.github.com>
2022-06-30 18:54:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name = "Raymon ";
+var second_name = "Suresh";
+var full_name = first_name.concat(second_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log(add);
+
+var sub = a-b;
+console.log(sub);
+var mul = a*b;
+console.log(mul);
+var divi = a/b;
+console.log(divi);
